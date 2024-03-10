3d34e76f9e0052f66e536eb1831a49d43cc1664b
toot_hzf <toot@tootzoe.com>
2024-03-10 18:37:06 +0700

    Initial Commit

---
+++ libtgd/libtgd.h
@@ -0,0 +1,24 @@
+//
+//  libtgd.h
+//  libtgd
+//
+//  Created by thor on 10/3/24
+//  
+//
+//  Email: toot@tootzoe.com  Tel: +855 69325538 
+//
+//
+
+
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for libtgd.
+FOUNDATION_EXPORT double libtgdVersionNumber;
+
+//! Project version string for libtgd.
+FOUNDATION_EXPORT const unsigned char libtgdVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libtgd/PublicHeader.h>
+
+
