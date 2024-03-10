829df15d877fb28216b48aa8e1aa632a5d811002
toot_hzf <toot@tootzoe.com>
2024-03-11 01:02:42 +0700

    Initial commit....

--- libtgd/libtgd.h
+++ libtgd/libtgd.h
@@ -1,11 +1,11 @@
+
 //
-//  libtgd.h
-//  libtgd
+ 
 //
 //  Created by thor on 10/3/24
-//  
 //
-//  Email: toot@tootzoe.com  Tel: +855 69325538 
+//
+//  Email: toot@tootzoe.com  Tel: +855 69325538
 //
 //
 
@@ -13,12 +13,14 @@
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for libtgd.
+ 
 FOUNDATION_EXPORT double libtgdVersionNumber;
 
-//! Project version string for libtgd.
+ 
 FOUNDATION_EXPORT const unsigned char libtgdVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <libtgd/PublicHeader.h>
+ 
 
-
+#import <libtgd/gd.h>
+#import <libtgd/gd_io.h>
+#import <libtgd/gdfx.h>
